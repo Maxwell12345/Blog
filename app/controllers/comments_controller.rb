bb2381f58ad00fa1db737a8d12f82d9253d85cea
marcugit <marcu@ualberta.ca>
2015-05-14 09:24:49 -0600

    can edit comments now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,34 @@ class CommentsController < ApplicationController
 	http_basic_authenticate_with name: "ana", password: "ana",
 	only: :destroy
 
+	def show
+    	@comment = Comment.find(params[:id])
+  	end
+
+  	def new
+    	@comment = Comment.new
+  	end
+
+  	def edit
+  		@comment = Comment.find(params[:id])
+  		@article = Article.find(params[:article_id])
+	end
+
+
 	def create
 		@article = Article.find(params[:article_id])
 		# create automatically link comment to belong to article
 		@comment = @article.comments.create(comment_params) #@article.comments hierarchy
 		redirect_to article_path(@article)
+	end
+
+	def update
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+
+		@comment.update_attributes(comment_params)
+		redirect_to @article
+		
 	end
 
 	def destroy
